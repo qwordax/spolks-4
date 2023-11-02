369f84bacd4000a7de3d4fca2adefe32fd3b0b32
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-11-02 15:26:52 +0300

    `feat:` implement client connection

--- client/main.py
+++ client/main.py
@@ -16,14 +16,24 @@ def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     atexit.register(sock.close)
 
-    while True:
-        args = input('> ').split()
+    try:
+        sock.settimeout(5)
+        sock.connect((address, port))
 
-        if args == []:
-            continue
+        while True:
+            args = input('> ').split()
 
-        if args[0] == 'quit':
-            break
+            if args == []:
+                continue
+
+            if args[0] == 'quit':
+                break
+    except ConnectionRefusedError:
+        print('error: connection refused', file=sys.stderr)
+        sys.exit(1)
+    except TimeoutError:
+        print('error: timeout', file=sys.stderr)
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()
